e1f53f6d9dba97597f5282af8b5a4fa7a81bde73
Ritooon <henri.vignaux@gmail.com>
2021-04-22 13:45:29 +0200

    Attributs de la classe Personnage en private

--- src/Entity/Personnage.php
+++ src/Entity/Personnage.php
@@ -3,10 +3,10 @@
 namespace App\Entity;
 
 class Personnage {
-    public $nom;
-    public $age;
-    public $sexe;
-    public $caracteristiques = [];
+    private $nom;
+    private $age;
+    private $sexe;
+    private $caracteristiques = [];
 
     public static $personnages = [];
 
@@ -21,10 +21,35 @@ class Personnage {
 
     public static function creerPersonnages()
     {
-        $p1 = new Personnage('hector', 25, 'M', ['force' => 5, 'agil' => 3, 'intel' => 1]);
-        $p2 = new Personnage('lena', 23, 'F', ['force' => 3, 'agil' => 3, 'intel' => 3]);
-        $p3 = new Personnage('robot', 99, '', ['force' => 5, 'agil' => 1, 'intel' => 8]);
-        $p4 = new Personnage('zombie', 99, '', ['force' => 2, 'agil' => 1, 'intel' => 1]);
+        new Personnage('hector', 25, 'M', ['force' => 5, 'agil' => 3, 'intel' => 1]);
+        new Personnage('lena', 23, 'F', ['force' => 3, 'agil' => 3, 'intel' => 3]);
+        new Personnage('robot', 99, '', ['force' => 5, 'agil' => 1, 'intel' => 8]);
+        new Personnage('zombie', 99, '', ['force' => 2, 'agil' => 1, 'intel' => 1]);
+    }
+
+    public function getNom() { return $this->nom; }
+    public function getAge() { return $this->age; }
+    public function getSexe() { return $this->sexe; }
+    public function getCaracteristiques() { return $this->caracteristiques; }
+
+    public function setNom($Nom) {
+        $this->Nom = $Nom;
+        return $this;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+        return $this;
+    }
+
+    public function setSexe($sexe) {
+        $this->sexe = $sexe;
+        return $this;
+    }
+
+    public function setCaracteristiques($caracteristiques) {
+        $this->caracteristiques = $caracteristiques;
+        return $this;
     }
 
     public static function getPersonnageParNom($nom)
